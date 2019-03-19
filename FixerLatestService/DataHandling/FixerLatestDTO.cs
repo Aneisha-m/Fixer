cebdd846c44707ea5fc5a835b48cd9bf6ac6f6bd
Aneisha Mallikaratchy <46450865+Aneisha-m@users.noreply.github.com>
2019-03-19 20:27:01 +0000

    initial commit

---
+++ FixerLatestService/DataHandling/FixerLatestDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Fixer
+{
+    public class FixerLatestDTO
+    {
+        public LatestRatesRoot LatestRates { get; set; }
+        public Rates Rates { get; set; }
+
+        public void DeserializeLatestRates(string LatestRatesResponse)
+        {
+            LatestRates = JsonConvert.DeserializeObject<LatestRatesRoot>(LatestRatesResponse);
+        }
+
+        
+
+        
+
+    }
+}
